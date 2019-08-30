a184eb0913e4b291b09000d94d32792030519707
marcelo oyaneder labarca <marcelo.oyaneder.l@gmail.com>
2019-08-30 10:16:36 -0400

    importing os
    import os

--- arboretum.py
+++ arboretum.py
@@ -1,10 +1,13 @@
 #package imports
 import pandas as pd
+import os
+import errno
 
 #creation of classes and functions
 
 def infowriting(ID,info):
-    with open(ID+'.txt','w') as fil:
+    filename = ID+'.txt'
+    with open(filename,'w') as fil:
         fil.write(str(info))
     pass
     return 
@@ -18,6 +21,7 @@ data=data.set_index("catalogNumber", drop = False)
 #obtaining ID data
 IDs=data['catalogNumber'].tolist()
 
+#verificar si los archivos existen, si es que existen comparar estos, si no crearlos.
 #get info into a txt document ESTA FUNCIONANDO NO EJECUTAR YA QUE CREA DEMASIADOS ARCHIVOS
-#for id in IDs:
-#    infowriting(id,data.loc[id])
+for id in IDs:
+    infowriting(id,data.loc[id])
