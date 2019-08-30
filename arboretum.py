60bc1671cb1132997bbd04bd9ce3fb267bbda81b
marcelo oyaneder labarca <marcelo.oyaneder.l@gmail.com>
2019-08-30 09:28:53 -0400

    Update arboretum.py
    Changes

--- arboretum.py
+++ arboretum.py
@@ -1,9 +1,23 @@
 #package imports
 import pandas as pd
 
+#creation of classes and functions
+
+def infowriting(ID,info):
+    with open(ID+'.txt','w') as fil:
+        fil.write(str(info))
+    pass
+    return 
+
+
 #Process
+#read excel
 data=pd.read_excel('Inventario_2019_Arboretum_Antumapu_Dwc.xlsx',sheet_name='Hoja1',header=0)
-data=data.set_index("catalogNumebr", drop = True)
+#rearrange dataframe
+data=data.set_index("catalogNumber", drop = False)
+#obtaining ID data
+IDs=data['catalogNumber'].tolist()
 
-print data.loc['AA1714','order']
-
+#get info into a txt document ESTA FUNCIONANDO NO EJECUTAR YA QUE CREA DEMASIADOS ARCHIVOS
+#for id in IDs:
+#    infowriting(id,data.loc[id])
