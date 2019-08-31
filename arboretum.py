69bbb625ddf76b2176c4a194159d36ec58a5dcb2
marcelo oyaneder labarca <marcelo.oyaneder.l@gmail.com>
2019-08-30 22:39:05 -0400

    implementacion de QR

--- arboretum.py
+++ arboretum.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import os
 import errno
+import pyqrcode
 
 #http://arboretum.oost-vlaanderen.be/index.cfm?nummer=00006251 IDEA TIPO
 #EL PASO SIGUIENTE ES CREAR UNA CARPETA EN QUE VAYAN LOS ARCHIVOS TXT, COMPARAR SI ES QUE EXISTEN EN ESTA, 
@@ -29,3 +30,9 @@ IDs=data['catalogNumber'].tolist()
 #get info into a txt document ESTA FUNCIONANDO NO EJECUTAR YA QUE CREA DEMASIADOS ARCHIVOS
 for id in IDs:
     infowriting(id,data.loc[id])
+
+#CREACION DE CODIGOS QR
+#url = pyqrcode.create('URL')
+#url.svg('uca-url.svg', scale=8)
+#url.eps('uca-url.eps', scale=2)
+#print(url.terminal(quiet_zone=1))
